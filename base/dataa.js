12ed273810aa7f833745dce3fbd74d8e9ab89c50
Pawan Rajbhar <109269775+ecompawan@users.noreply.github.com>
2024-01-10 11:10:13 +0000

    datatypes

---
+++ base/dataa.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version 
+
+// alert("hello") // we are using node js not browser
+
+let age = 22
+let Name = "pawan"
+let city;
+
+//number => 2 to power of 25
+//bigint
+//string => ""
+//boolean => true/false
+//null => standlonevalue
+//undefined =>
+//symbol => unique
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // indefined
